8b06bf7f5c1f3820279147a282a21d9e0f3a1585
Franck Demathieu <demathif@deneb.nc.intel.com>
2016-04-16 09:39:43 +0200

    gpx: helper to merge gpx and loc files
    Signed-off-by: Franck Demathieu <demathif@deneb.nc.intel.com>

---
+++ geo2gpx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $# -ne 3 ]]; then
+	echo "usage: geo2gpx LOC_DIR IN_GPX OUT_GPX" 
+	exit
+fi
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+LOC_FILES=$1/*.loc
+cmd="gpsbabel -i geo"
+for f in $LOC_FILES
+do
+	cmd="$cmd -f '$f'"
+done
+
+cmd="$cmd -i gpx -f $2 -o gpx -F $3"
+
+#echo $cmd
+eval $cmd
+
+IFS=$SAVEIFS
