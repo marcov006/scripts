d27f8ac923b2d221d78ef392e7edb8068bd973db
Franck Demathieu <fdemathieu@gmail.com>
2017-04-19 11:23:05 +0200

    fixup! qemu: buildroot quick launcher (x86_64)

--- run-qemu.sh
+++ run-qemu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-BR=/work/buildroot
+if [ "${1:-unset}" != 'unset' ];
+then
+	BR=$1
+else
+	BR=/work/buildroot
+fi
 
 qemu-system-x86_64 -M pc -kernel $BR/output/images/bzImage -drive file=$BR/output/images/rootfs.ext2,if=virtio,format=raw -append 'console=ttyS0 root=/dev/vda' -net nic,model=virtio -net user -nographic
